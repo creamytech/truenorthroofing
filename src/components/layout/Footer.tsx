4814087559787767fcb709157c1c45570be4b70e
creamytech <benjaminscott18@gmail.com>
2026-02-05 23:43:24 -0500

    Increase footer bottom padding for mobile sticky CTA

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -63,7 +63,7 @@ export function Footer() {
         
         <div className="relative">
           {/* Main Footer */}
-          <div className="container mx-auto px-4 lg:px-8 py-16 pb-24 lg:pb-16">
+          <div className="container mx-auto px-4 lg:px-8 py-16 pb-32 lg:pb-16">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
               {/* Brand Column */}
               <div className="lg:col-span-1 text-center md:text-left">
